85941990029fc214b463c76732d1dc5cef6912d4
jsk_pub <jsk.pubwork@gmail.com>
2022-11-09 15:41:29 +0900

    커뮤니티, 검진 예약 외 추가 수정 및 scss 파일 변경

---
+++ dist/js/date.js
@@ -0,0 +1,24 @@
+"use strict";
+
+$.datepicker.setDefaults({
+  closeText: '닫기',
+  currentText: '오늘',
+  prevText: '이전 달',
+  nextText: '다음 달',
+  monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+  monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+  dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+  dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+  weekHeader: '주',
+  yearSuffix: '년',
+  dateFormat: 'yy-mm-dd',
+  showMonthAfterYear: true,
+  showOtherMonths: true,
+  showButtonPanel: true,
+  autoSize: true,
+  changeMonth: true,
+  changeYear: true,
+  buttonImageOnly: true,
+  minDate: 0
+});
+//# sourceMappingURL=maps/date.js.map
