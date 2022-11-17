1a686e1823036126969b659422b5857ac674eda3
jsk_pub <jsk.pubwork@gmail.com>
2022-11-17 18:15:02 +0900

    datepicker js 추가

---
+++ dist/js/date02.js
@@ -0,0 +1,25 @@
+"use strict";
+
+$.datepicker.setDefaults({
+  closeText: '닫기',
+  prevText: '이전 달',
+  nextText: '다음 달',
+  monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+  monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+  yearNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+  dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+  dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+  weekHeader: '주',
+  yearSuffix: '년',
+  dateFormat: 'yy-mm-dd',
+  showMonthAfterYear: true,
+  showOtherMonths: true,
+  autoSize: true // buttonImageOnly: true,
+  // minDate: 0,
+
+});
+$('#c-datepicker').datepicker({});
+$(function () {
+  var _updatePicker = $.datepicker._updateDatepicker;
+});
+//# sourceMappingURL=maps/date02.js.map
