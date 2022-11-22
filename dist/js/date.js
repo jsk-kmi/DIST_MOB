1085f1002acc5917228141d15142d200241aa1ba
jsk_pub <jsk.pubwork@gmail.com>
2022-11-22 15:31:04 +0900

    datepicker 수정 사항 반영

--- dist/js/date.js
+++ dist/js/date.js
@@ -4,13 +4,13 @@ $.datepicker.setDefaults({
   closeText: '닫기',
   prevText: '이전 달',
   nextText: '다음 달',
-  monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-  monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+  monthNames: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+  monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
   yearNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
   dayNames: ['일', '월', '화', '수', '목', '금', '토'],
   dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
   weekHeader: '주',
-  yearSuffix: '년',
+  // yearSuffix: '년',
   dateFormat: 'yy-mm-dd',
   showMonthAfterYear: true,
   showOtherMonths: true,
@@ -21,12 +21,4 @@ $.datepicker.setDefaults({
 
 });
 $('#c-datepicker').datepicker({});
-$(document).on('click', '.ui-datepicker-next, .ui-datepicker-prev', function () {
-  var dateHeaderTitle = $(this).siblings('.ui-datepicker-title').html(),
-      dateHeaderText = $(this).siblings('.ui-datepicker-title').text(); // Year Title Control
-
-  if (dateHeaderTitle.match('ui-datepicker-yearpicker')) {
-    $('.ui-datepicker-yearpicker').text(dateHeaderText + '년');
-  }
-});
 //# sourceMappingURL=maps/date.js.map
